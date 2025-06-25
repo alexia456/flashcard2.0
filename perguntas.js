103a67cf0952fd18a304106bb0c3b59762e884f0
alexia456 <alexiadelfinodeoliveira30@gmail.com>
2025-06-25 17:13:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'historia',
+    'O que foi o imperialismo no séc xix?',
+    'Dominio de países europeus sobre territorios na áfrica,ásia e oceania.'
+)
+
+criaCartao(
+    'historia',
+    'Quais países lideram o imperialismo?',
+    'Reino Unido, França, Alemanha, Belgica,Itália,Eua e Japãos.'
+)
+
+criaCartao(
+    'historia',
+    'O que foi a conferência de Berlim?',
+    'Reunião que dividiu a África entre os países europeus.'
+)
+
+criaCartao(
+    'historia',
+    'Como os povos colonizados foram afetados?',
+    'Foram explorados, perderam culturas e sofreram violência.'
+)
+criaCartao(
+    'historia',
+    'O que é Darwinismo social?',
+    'Ideia usada  para justificar a superioridade europeia'
+)
+criaCartao(
+    'historia',
+    'Qual a ligação com a Revolução Industrial?',
+    'Aumenta a busca por matérias-primas e mercados.'
+)
